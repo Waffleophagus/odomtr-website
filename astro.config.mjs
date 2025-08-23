ffb61775a60e8dc5b49bfb7afe1ee2dde0da4f0b
Matt Campbell <matt@battleshopper.com>
2025-08-23 16:21:34 -0500

    updating workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: 'https://waffleophagus.github.io',
+  base: '/odomtr-website',
   integrations: [tailwind(), react()],
 });
